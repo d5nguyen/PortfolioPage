fdbed85705721855fe83ef2de1298bb37dcdc0aa
d5nguyen <d5nguyen@gmail.com>
2019-02-08 23:18:02 -0500

    css maneuvering

---
+++ resources/js/portfolio.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+
+//   $('.js--sections-project').waypoint(function(direction){
+//     if (direction == "down") {
+//       $('.main-nav').addClass('sticky');
+//     } else {
+//       $('.main-nav').removeClass('sticky');
+//     }
+//   });
+
+// });
