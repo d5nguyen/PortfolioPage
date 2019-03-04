567de8213b83d4cb25e1ed5498175b6f9f659cf9
d5nguyen <d5nguyen@gmail.com>
2019-03-04 00:29:04 -0500

    js

---
+++ resources/portfolio.js
@@ -0,0 +1,11 @@
+$(window).on('scroll', function(){
+    if($(window).scrollTop()){
+        $('nav').addClass('sticky');
+    } else {
+        $('nav').removeClass('sticky');
+    }
+}); 
+
+function scrollToElement(){
+    
+}
