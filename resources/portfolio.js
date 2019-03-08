dc308409aedb7ce91eabd9d2ecece47a7910ef62
d5nguyen <d5nguyen@gmail.com>
2019-03-08 00:42:12 -0500

    chance icon + js

--- resources/portfolio.js
+++ resources/portfolio.js
@@ -1,11 +1,16 @@
-$(window).on('scroll', function(){
-    if($(window).scrollTop()){
-        $('nav').addClass('sticky');
-    } else {
-        $('nav').removeClass('sticky');
-    }
-}); 
+// $(window).on('scroll', function(){
+//     if($(window).scrollTop()){
+//         $('nav').addClass('sticky');
+//     } else {
+//         $('nav').removeClass('sticky');
+//     }
+// }); 
 
-function scrollToElement(){
-    
+function smoothScroll(target, duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    console.log(targetPosition);
 }
+
+
+smoothScroll('.js--projects', 1000);
